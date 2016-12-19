843cded86898c1403dcda29f53e13852aa502dfd
Amir.h Yeganemehr <yeganemehr@yahoo.com>
2016-12-19 23:10:38 +0330

    Fix #1

--- frontend-website/views/about.php
+++ frontend-website/views/about.php
@@ -1,11 +1,14 @@
 <?php
 namespace themes\musixmatch\views;
 use \packages\base;
+use \packages\base\translator;
 use \themes\musixmatch\viewTrait;
 use \packages\ghafiye\views\about as aboutus;
 class about extends aboutus{
 	use viewTrait;
 	function __beforeLoad(){
-		$this->setTitle("در باره ما");
+		$this->setTitle(translator::trans('about.title'));
+		$this->addBodyClass('about');
+		$this->addBodyClass('article');
 	}
 }
