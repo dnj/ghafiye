7280d864f7f2d12effd36eb29327d31adbb01170
Mahdi Abedi <abedi@jeyserver.com>
2017-07-17 11:45:53 +0430

    use avatar preview in add new gruop

--- frontend_panel/html/groups/add.php
+++ frontend_panel/html/groups/add.php
@@ -1,17 +1,8 @@
 <?php
-use \packages\base;
 use \packages\base\json;
-use \packages\base\http;
+use \packages\base\frontend\theme;
 use \packages\base\translator;
-use \packages\base\db\dbObject;
-use \packages\base\views\FormError;
-
 use \packages\userpanel;
-use \packages\userpanel\date;
-
-use \themes\clipone\utility;
-
-use \packages\gamakey\plan;
 
 $this->the_header();
 ?>
@@ -28,36 +19,22 @@ $this->the_header();
 	        <div class="panel-body">
 	            <div class="table-responsive">
 	                <form class="group_add_form" action="<?php echo userpanel\url('groups/add'); ?>" method="post" enctype="multipart/form-data">
-						<div class="col-md-5">
-							<div class="form-group">
-								<label class="control-label"><?php echo translator::trans("ghafiye.panel.group.avatar"); ?></label>
-								<div class="center group-image-box">
-									<div class="fileupload fileupload-new" data-provides="fileupload">
-										<div class="group-image">
-											<div class="fileupload-new thumbnail">
-										        <img src="<?php echo $this->getImage(); ?>" alt="groupImage">
-										    </div>
-											<div class="fileupload-preview fileupload-exists thumbnail"></div>
-										    <div class="group-image-buttons">
-										        <span class="btn btn-teal btn-file btn-sm">
-											        <span class="fileupload-new">
-											        	<i class="fa fa-pencil"></i>
-										            </span>
-											        <span class="fileupload-exists">
-											            <i class="fa fa-pencil"></i>
-								                  	</span>
-											        <input name="avatar" type="file">
-										        </span>
-										        <a href="#" class="btn fileupload-exists btn-bricky btn-sm" data-dismiss="fileupload">
-										            <i class="fa fa-times"></i>
-										        </a>
-										    </div>
+						<div class="col-sm-3">
+							<label class="control-label"><?php echo translator::trans("ghafiye.panel.group.avatar"); ?></label>
+							<div class="fileupload fileupload-new" data-provides="fileupload">
+								<div class="form-group">
+									<div class="user-image avatarPreview">
+										<img src="<?php echo $this->getImage(); ?>" class="preview img-responsive">
+										<input name="avatar" type="file">
+										<div class="button-group">
+											<button type="button" class="btn btn-teal btn-sm btn-upload"><i class="fa fa-pencil"></i></button>
+											<button type="button" class="btn btn-bricky btn-sm btn-remove" data-default="<?php echo theme::url('assets/images/defaultavatar.jpg'); ?>"><i class="fa fa-times"></i></button>
 										</div>
 									</div>
 								</div>
 							</div>
 	                    </div>
-						<div class="col-md-7">
+						<div class="col-sm-9">
 							<?php $this->createField(array(
 								'name' => 'group-lang',
 								'type' => 'select',
@@ -71,10 +48,10 @@ $this->the_header();
 							));
 							?>
 						</div>
-						<div class="col-md-12">
+						<div class="col-sm-12">
 			                <p>
 			                    <a href="<?php echo userpanel\url('groups'); ?>" class="btn btn-light-grey"><i class="fa fa-chevron-circle-right"></i> <?php echo translator::trans('return'); ?></a>
-			                    <button type="submit" class="btn btn-yellow"><i class="fa fa-check-square-o"></i> <?php echo translator::trans("confrim") ?></button>
+			                    <button type="submit" class="btn btn-success"><i class="fa fa-check-square-o"></i> <?php echo translator::trans("confrim") ?></button>
 			                </p>
 						</div>
 					</form>
