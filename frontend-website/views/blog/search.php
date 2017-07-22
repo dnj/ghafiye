a35e8b745d997ae9f8414281553ff89c98e0abae
Mahdi Abedi <abedi@jeyserver.com>
2017-07-22 17:02:18 +0430

    blog posts search

---
+++ frontend-website/views/blog/search.php
@@ -0,0 +1,18 @@
+<?php
+namespace themes\musixmatch\views\blog;
+use \packages\base\translator;
+use \themes\musixmatch\viewTrait;
+use \themes\musixmatch\views\blogTrait;
+use \themes\musixmatch\views\formTrait;
+use \packages\blog\views\post\search as postSearch;
+class search extends postSearch{
+	use viewTrait, blogTrait, formTrait;
+	function __beforeLoad(){
+		$this->setTitle(array(
+			translator::trans("blog.search.title", ['word'=>$this->getWord()]),
+		));
+		$this->addBodyClass('article');
+		$this->addBodyClass('blog');
+		$this->addBodyClass('search');
+	}
+}
