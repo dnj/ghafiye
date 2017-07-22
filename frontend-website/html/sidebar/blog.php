1f0515d921c1391c9fcdc87c337e1d8394dc0210
Mahdi Abedi <abedi@jeyserver.com>
2017-07-22 16:57:34 +0430

    categories and search in blog sidebar

--- frontend-website/html/sidebar/blog.php
+++ frontend-website/html/sidebar/blog.php
@@ -4,20 +4,54 @@ use \packages\base\translator;
 use \packages\userpanel\date;
 ?>
  <div class="col-md-3 sidebar">
+	 <?php if($subjects = $this->getPostsSubJects()){ ?>
+	 <div class="panel panel-default">
+		<div class="panel-heading"><?php echo translator::trans('blog.sidebar.subjects'); ?></div>
+		<div class="panel-body">
+			<ul class="categories">
+				<?php echo $this->getPostsSubJects(); ?>
+			</ul>
+		</div>
+	</div>
+	<?php } ?>
+	<?php if($popularsPost = $this->popular_blog()){ ?>
 	 <div class="panel panel-default">
 		<div class="panel-heading"><?php echo translator::trans('blog.popular_blog'); ?></div>
 		<ul class="list-group">
-			<?php foreach($this->popular_blog() as $popular){ ?>
-				<a href="<?php echo base\url("blog/view/".$popular->id); ?>" class="list-group-item"><?php echo($popular->title); ?></a>
+			<?php foreach($popularsPost as $popular){ ?>
+				<li><a href="<?php echo base\url("blog/view/".$popular->id); ?>" class="list-group-item"><?php echo($popular->title); ?></a></li>
 			<?php } ?>
 		</ul>
 	</div>
+	<?php } ?>
+	<?php if($archivesPosts = $this->archive_box()){ ?>
 	 <div class="panel panel-default">
 		 <div class="panel-heading"><?php echo translator::trans('blog.archive_box'); ?></div>
 		 <ul class="list-group">
-			 <?php foreach($this->archive_box() as $archive){ ?>
-				 <a href="<?php echo base\url("blog/archive/".date::format("Y/m", $archive)); ?>" class="list-group-item"><?php echo(date::format("F Y", $archive)); ?></a>
+			 <?php foreach($archivesPosts as $archive){ ?>
+				 <li><a href="<?php echo base\url("blog/archive/".date::format("Y/m", $archive)); ?>" class="list-group-item"><?php echo(date::format("F Y", $archive)); ?></a></li>
 			 <?php } ?>
 		 </ul>
+	 </div>
+	<?php } ?>
+	 <div class="panel panel-default">
+		<div class="panel-heading"><?php echo translator::trans('blog.search'); ?></div>
+		<div class="panel-body">
+			<form action="<?php echo base\url('blog/search'); ?>" method="GET">
+				<?php $this->createField([
+					'name' => 'word',
+					'input-group' => [
+						'right' => [
+							[
+								'text' => '',
+								'icon' => 'fa fa-search',
+								'type' => 'submit',
+								'class' => ['btn', 'btn-sm', 'btn-teal']
+							]
+						]
+					]
+				]); ?>
+			</form>
+		</div>
 	 </div>
  </div>
--- frontend-website/libraries/mvc/views/blog.php
+++ frontend-website/libraries/mvc/views/blog.php
@@ -4,20 +4,23 @@ use \packages\base;
 use \packages\base\options;
 use \packages\base\packages;
 use \packages\blog\post;
-
 use \packages\userpanel\date;
-
+use \packages\blog\category;
 trait blogTrait{
 	protected $popular = array();
 	protected $blog = array();
-	public function popular_blog(){
+	private $categories;
+	function __construct(){
+		$this->categories = category::get();
+	}
+	public function popular_blog():array{
 		$new = new post();
+		$new->where('status', post::published);
 		$new->orderBy('view', 'DESC');
 		return $new->get(4);
 	}
-	public function archive_box(){
-
-		$posts = post::orderBy('date', 'DESC')->ArrayBuilder()->get(null, array('date'));
+	public function archive_box():array{
+		$posts = post::where('status', post::published)->orderBy('date', 'DESC')->ArrayBuilder()->get(null, array('date'));
 		$arcive = array();
 		foreach($posts as $post){
 			$arcive[] = date::mktime(0, 0, 0, date::format('m', $post['date']), 1,date::format('Y', $post['date']));
@@ -26,5 +29,21 @@ trait blogTrait{
 	}
 	function blogImage(post $post){
 		return (packages::package('blog')->url($post->image ? $post->image : options::get('packages.blog.defaultimage')));
+	}
+	public function getPostsSubJects($parent = null){
+		$html = "";
+		foreach($this->categories as $category){
+			if($category->parent == $parent){
+				$html .= "<li> <a href=\"".$category->getURL()."\">{$category->title}</a>";
+				$children = $this->getPostsSubJects($category->id);
+				if($children){
+					$html .= "<ul class=\"child\">";
+					$html .= $children;
+					$html .= "</ul>";
+				}
+				$html .= "</li>";
+			}
+		}
+		return $html;
 	}
 }
