d74efd8886e1706780c8b5e269b17bf0366da3c3
Mahdi Abedi <abedi@jeyserver.com>
2017-07-22 16:53:28 +0430

    generate errors html

--- frontend-website/libraries/mvc/view.php
+++ frontend-website/libraries/mvc/view.php
@@ -1,5 +1,7 @@
 <?php
 namespace themes\musixmatch;
+use \packages\base\view\error;
+use \packages\base\translator;
 use \packages\news\newpost;
 trait viewTrait{
 	protected $bodyClasses = array('rtl');
@@ -22,5 +24,71 @@ trait viewTrait{
 	}
 	protected function genBodyClasses(){
 		return implode(' ', $this->bodyClasses);
+	}
+	protected function getErrorsHTML(){
+		$code = '';
+		foreach($this->getErrors() as $error){
+			$alert = array(
+				'type' => 'info',
+				'txt' => $error->getMessage(),
+				'title' => ''
+			);
+			$data  = $error->getData();
+			if(!is_array($data)){
+				$data = array();
+			}
+			$alert = array_merge($alert, $data);
+			if(!$alert['txt']){
+				if($translator = translator::trans('error.'.$error->getCode())){
+					$alert['txt'] = $translator;
+				}else{
+					$alert['txt'] = $error->getCode();
+				}
+			}
+			switch($error->getType()){
+				case(error::FATAL):
+					$alert['type'] = 'danger';
+					if(!$alert['title'])
+						$alert['title'] = translator::trans('error.'.error::FATAL.'.title');
+					break;
+				case(error::WARNING):
+					$alert['type'] = 'warning';
+					if(!$alert['title'])
+						$alert['title'] = translator::trans('error.'.error::WARNING.'.title');
+					break;
+				case(error::NOTICE):
+					$alert['type'] = 'info';
+					if(!$alert['title'])
+						$alert['title'] = translator::trans('error.'.error::NOTICE.'.title');
+					break;
+			}
+			if(isset($alert['classes']) and is_array($alert['classes'])){
+				$alert['classes'] = implode(" ", $alert['classes']);
+			}else{
+				$alert['classes'] = '';
+			}
+			$code .= "<div class=\"alert alert-block alert-{$alert['type']} {$alert['classes']}\">";
+			$code .= "<button data-dismiss=\"alert\" class=\"close\" type=\"button\">&times;</button>";
+			$code .= "<h4 class=\"alert-heading\">";
+			switch($alert['type']){
+				case('danger'):$code.="<i class=\"fa fa-times-circle\"></i>";break;
+				case('success'):$code.="<i class=\"fa fa-check-circle\"></i>";break;
+				case('info'):$code.="<i class=\"fa fa-info-circle\"></i>";break;
+				case('warning'):$code.="<i class=\"fa fa-exclamation-triangle\"></i>";break;
+			}
+
+			$code .= " {$alert['title']}</h4><p>{$alert['txt']}</p>";
+
+			if(isset($alert['btns']) and $alert['btns']){
+				$code .= "<p>";
+				foreach($alert['btns'] as $btn){
+					$code .= "<a href=\"{$btn['link']}\" class=\"btn {$btn['type']}\">{$btn['txt']}</a> ";
+				}
+				$code .= "</p>";
+			}
+			$code .= "</div>";
+		}
+
+		return $code;
 	}
 }
