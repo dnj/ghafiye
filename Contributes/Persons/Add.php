c3c3f6b3bfd839dba5653f463c8f154e3855b1ce
Mahdi Abedi <abedi@jeyserver.com>
2018-08-09 11:47:48 +0430

    Fix #54

--- Contributes/Persons/Add.php
+++ Contributes/Persons/Add.php
@@ -1,7 +1,7 @@
 <?php
 namespace packages\ghafiye\contributes\persons;
 use packages\base;
-use packages\base\{db, view\error};
+use packages\base\{db, view\error, translator};
 use packages\ghafiye\{Contributes, person, song, Contribute, contributes\preventRejectException};
 
 class Add extends Contributes {
@@ -16,7 +16,25 @@ class Add extends Contributes {
 		return "<a href=\"" . base\url($this->contribute->person->encodedName()) . "\">{$this->contribute->person->name($this->contribute->lang)}</a>";
 	}
 	public function buildFrontend(): string {
-		$html = "";
+		$html = '<div class="row">
+			<div class="col-sm-4 col-xs-12">
+				<img class="img-responsive" src="' . $this->contribute->person->getImage(250, 250) . '" alt="' . $this->contribute->person->name($this->contribute->lang) . '">
+			</div>
+			<div class="col-sm-8 col-xs-12">
+				<div class="row">
+					<div class="col-xs-12">
+						<div class="form-group">
+							<label class="col-xs-5">' . translator::trans("ghafiye.person.name") .':</label>
+							<div class="col-xs-7">' . $this->contribute->person->name($this->contribute->lang) . '</div>
+						</div>
+						<div class="form-group">
+							<label class="col-xs-5">' . translator::trans("ghafiye.person.lang") .':</label>
+							<div class="col-xs-7">' . translator::trans("translations.langs.{$this->contribute->lang}") . '</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>';
 		return $html;
 	}
 	public function onAccept() {
